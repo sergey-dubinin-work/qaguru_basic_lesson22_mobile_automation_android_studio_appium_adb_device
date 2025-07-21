01116fdd57d2197ed8c3cfc54021ba948d371498
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-07-21 15:50:03 +0300

    add onboarding test

--- src/test/java/guru/qa/tests/AndroidSelenideTests.java
+++ src/test/java/guru/qa/tests/AndroidSelenideTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
@@ -38,4 +39,57 @@ public class AndroidSelenideTests extends TestBase{
 
     }
 
+    @Test
+    void onboardingTest() {
+
+        step("Check Onboarding. Step 1", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(text("The Free Encyclopedia"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(text("in over 300 languages"));
+        });
+
+        step("Onboarding. Continue", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button"))
+                    .shouldHave(exactText("Continue"))
+                    .click();
+        });
+
+        step("Check Onboarding. Step 2", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(exactText("New ways to explore"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView")).shouldHave(text("Dive down the Wikipedia rabbit hole with a constantly updating Explore feed"));
+        });
+
+        step("Onboarding. Continue", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button"))
+                    .shouldHave(exactText("Continue"))
+                    .click();
+        });
+
+        step("Check Onboarding. Step 3", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(exactText("Reading lists with sync"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView")).shouldHave(text("You can make reading lists from articles you want to read later, even when you’re offline"));
+        });
+
+        step("Onboarding. Continue", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button"))
+                    .shouldHave(exactText("Continue"))
+                    .click();
+        });
+
+        step("Check Onboarding. Step 4", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(exactText("Data & Privacy"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView")).shouldHave(text("We believe that you should not have to provide personal information to participate in the free knowledge movement"));
+        });
+
+        step("Onboarding. Finish", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_done_button"))
+                    .shouldHave(exactText("Get started"))
+                    .click();
+        });
+
+        step("Main page appeared", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/main_toolbar_wordmark"))
+                    .should(exist);
+        });
+
+    }
 }
